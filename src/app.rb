fb6e9a8748788448ae08ddf55c2bf967d9eacbb3
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-12-09 21:35:29 -0500

    created key handle event

--- src/app.rb
+++ src/app.rb
@@ -5,12 +5,12 @@ require_relative 'actions/actions'
 class App
   def initialize
     @state = Model::initial_state
+    @view = View::Ruby2dView.new(self)
   end
 
   def start
-    view = View::Ruby2dView.new
-    Thread.new { init_timer(view)}
-    view.start(@state)
+    Thread.new { init_timer(@view)}
+    @view.start(@state)
   end
 
   def init_timer(view)
@@ -18,6 +18,14 @@ class App
       @state = Actions::move_snake(@state)
       view.render(@state)
       sleep(0.5)
+    end
+  end
+
+  def send_action(action, params)
+    new_state = Actions.send(action, @state, params)
+    if new_state.hash != @state
+      @state = new_state
+      @view.render(@state)
     end
   end
 end
